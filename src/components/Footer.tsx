23b71f8a00b73049bc3434fd872079be1bf2164b
Samuel Ajayi <samuelajayi554@gmail.com>
2025-06-10 15:25:52 +0100

    updated social icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,19 +5,19 @@ const Footer = () => {
             <div className='flex'>
                 <img 
                  className='w-6 h-6'
-                src="./src/assets/images/social-icons/mail-fill.svg" 
+                src="/images/social-icons/mail-fill.svg" 
                 alt="twitter-icon" 
                 />
 
                 <img 
                 className='w-6 h-6'
-                src="./src/assets/images/social-icons/linkedin-box-fill.svg" 
+                src="/images/social-icons/linkedin-box-fill.svg" 
                 alt="linkedin-icon" 
                 />
 
                 <img 
                 className='w-6 h-6'
-                src="./src/assets/images/social-icons/twitter-x-line.svg" 
+                src="/images/social-icons/twitter-x-line.svg" 
                 alt="linked-icon" 
                 />
 
